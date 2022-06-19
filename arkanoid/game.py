3c10a3c7af8f2667a514fc08132e3e38b86c8c3b
Luisda <lsanchezroldan@gmail.com>
2022-06-19 17:21:35 +0200

    Establecemos pantalla

--- arkanoid/game.py
+++ arkanoid/game.py
@@ -1,6 +1,11 @@
+import pygame
+
+from arkanoid import ALTO, ANCHO
 class Arkanoid:
     def __init__(self) -> None:
         print("Arranaca el juego!!")
+        pygame.init()
+        pygame.display.set_mode((ANCHO, ALTO))
 
 if __name__ == "__main__":
     Arkanoid()
