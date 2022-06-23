72875662074c026dadaafcfa9c49c96c25e4469b
Luisda <lsanchezroldan@gmail.com>
2022-06-23 18:55:00 +0200

    Antes de corregir

---
+++ arkanoid/entidades.py
@@ -0,0 +1,47 @@
+import os
+
+import pygame as pg
+from pygame.sprite import Sprite
+
+from . import ALTO, ANCHO
+
+"""
+1. Crear una clase Raqueta
+   a. Sea un Sprite --- hecho
+   b. El método update es el que se encarga de gestionarla --- ya sabemos lo que hace
+2. Situarlo con las coordenadas y para eso, obtener el rectángulo ---
+3. Método mostrar_paleta ----
+4. En el bucle principal llamar a mostrar_paleta
+Para animar las imágenes:
+1. Función "animar" con una lista de imágenes y las mostramos en bucle
+"""
+
+
+class Raqueta(Sprite):
+
+    margen_inferior = 20
+
+    def __init__(self):
+        super().__init__()
+
+        self.sprites = [
+            pg.image.load(os.path.join(
+                "resources", "images", "electric00.png")),
+            pg.image.load(os.path.join(
+                "resources", "images", "electric01.png")),
+            pg.image.load(os.path.join(
+                "resources", "images", "electric02.png")),
+        ]
+        self.contador = 0
+
+        image_path = os.path.join("resources", "images", "electric00.png")
+        self.image = pg.image.load(image_path)
+        self.rect = self.image.get_rect(
+            midbottom=(ANCHO/2, ALTO-self.margen_inferior))
+
+    def update(self):
+        # self.rect.x = self.rect.x + 1
+        self.image = self.sprites[self.contador]
+        self.contador += 1
+        if self.contador > 2:
+            self.contador = 0
--- arkanoid/escenas.py
+++ arkanoid/escenas.py
@@ -2,12 +2,15 @@ import os
 
 import pygame as pg
 
-from . import ANCHO, ALTO, COLOR_FONDO_PANTALLA, COLOR_MENSAJE
+from . import ANCHO, ALTO, COLOR_FONDO_PORTADA, COLOR_FONDO_PORTADA, COLOR_MENSAJE, FPS
+
+from . entidades import Raqueta
 
 
 class Escena:
     def __init__(self, pantalla: pg.Surface):
         self.pantalla = pantalla
+        self.reloj = pg.time.Clock()
 
     def bucle_principal(self):
         pass
@@ -17,22 +20,23 @@ class Portada(Escena):
     def __init__(self, pantalla: pg.Surface):
         super().__init__(pantalla)
 
-        self.logo = pg.image.load(os.path.join(
-            "resources", "images", "arkanoid_name.png"))
+        self.logo = pg.image.load(
+            os.path.join("resources", "images", "arkanoid_name.png"))
 
         font_file = os.path.join("resources", "fonts", "CabinSketch-Bold.ttf")
-
         self.tipografia = pg.font.Font(font_file, 40)
 
     def bucle_principal(self):
         salir = False
+
         while not salir:
             for event in pg.event.get():
-                if event.type == pg.KEYDOWN and event.type == pg.K_SPACE:
+                if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
                     salir = True
                 if event.type == pg.QUIT:
                     pg.quit()
-            self.pantalla.fill((99, 0, 0))
+
+            self.pantalla.fill(COLOR_FONDO_PORTADA)
 
             self.pintar_logo()
             self.pintar_texto()
@@ -49,8 +53,8 @@ class Portada(Escena):
         mensaje = "Pulsa espacio para empezar"
         texto = self.tipografia.render(mensaje, True, COLOR_MENSAJE)
         ancho_texto = texto.get_width()
-        pos_x = (ANCHO - ancho_texto) / 2
-        pos_y = 3/4 * ALTO
+        pos_x = (ANCHO - ancho_texto) / 2        # ANCHO/2 - ancho_texto/2
+        pos_y = .75 * ALTO
         self.pantalla.blit(texto, (pos_x, pos_y))
 
 
@@ -68,14 +72,25 @@ class Partida(Escena):
         super().__init__(pantalla)
         bg_file = os.path.join("resources", "images", "background.jpg")
         self.fondo = pg.image.load(bg_file)
+        self.jugador = Raqueta()
 
     def bucle_principal(self):
         salir = False
         while not salir:
+            self.reloj.tick(FPS)
+            self.jugador.update()
+
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     pg.quit()
-            self.pantalla.fill((0, 99, 0))
+
+            #self.pantalla.fill((0, 99, 0))
+
+            self.pantalla.fill((0, 0, 66))
+            self.pintar_fondo()
+
+            self.pantalla.blit(self.jugador.image, self.jugador.rect)
+
             pg.display.flip()
 
     def pintar_fondo(self):
